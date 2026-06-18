ea2e89a75e6cc30247c23ec0cdb025517ac7f00f
2521003628-netizen <2521003628@sv.ufm.edu.vn>
2026-06-18 15:17:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,68 @@
 
 import streamlit as st
 
-st.title(" APP TÍNH TIỀN GỬI TIẾT KIỆM TẠI NGÂN HÀNG_NGUYỄN CÔNG DANH")
+st.title(" Ứng dụng tính Thuế Thu Nhập Cá Nhân")
 
-C = st.number_input(
-    "Nhập số tiền khách hàng gửi tiết kiệm (triệu đồng)",
+# Nhập thu nhập chịu thuế
+thu_nhap = st.number_input(
+    "Nhập thu nhập chịu thuế (triệu đồng/tháng)",
     min_value=0.0,
-    value=100.0
+    value=20.0
 )
 
-i = st.number_input(
-    "Nhập lãi suất gửi tiết kiệm theo năm (%)",
-    min_value=0.0,
-    value=6.0
-)
+if st.button("Tính thuế"):
 
-n = st.number_input(
-    "Nhập số tháng khách hàng gửi tiết kiệm",
-    min_value=1,
-    value=12
-)
+    if thu_nhap <= 5:
+        thue = thu_nhap * 0.05
 
+    elif thu_nhap <= 10:
+        thue = 5 * 0.05 + (thu_nhap - 5) * 0.10
 
-i = i / 100
+    elif thu_nhap <= 18:
+        thue = 5 * 0.05 + 5 * 0.10 + (thu_nhap - 10) * 0.15
 
+    elif thu_nhap <= 32:
+        thue = (
+            5 * 0.05
+            + 5 * 0.10
+            + 8 * 0.15
+            + (thu_nhap - 18) * 0.20
+        )
 
-if st.button("Tính toán"):
-    
-    # Lãi đơn
-    An = C * (1 + (i / 12) * n)
+    elif thu_nhap <= 52:
+        thue = (
+            5 * 0.05
+            + 5 * 0.10
+            + 8 * 0.15
+            + 14 * 0.20
+            + (thu_nhap - 32) * 0.25
+        )
 
-    # Lãi kép
-    Bn = C * (1 + i / 12) ** n
+    elif thu_nhap <= 80:
+        thue = (
+            5 * 0.05
+            + 5 * 0.10
+            + 8 * 0.15
+            + 14 * 0.20
+            + 20 * 0.25
+            + (thu_nhap - 52) * 0.30
+        )
+
+    else:
+        thue = (
+            5 * 0.05
+            + 5 * 0.10
+            + 8 * 0.15
+            + 14 * 0.20
+            + 20 * 0.25
+            + 28 * 0.30
+            + (thu_nhap - 80) * 0.35
+        )
+
+    thu_nhap_sau_thue = thu_nhap - thue
 
     st.success("Kết quả tính toán")
 
-    st.write(
-        f"Số tiền khách hàng nhận được theo lãi đơn: **{An:,.2f} triệu đồng**"
-    )
-
-    st.write(
-        f"Số tiền khách hàng nhận được theo lãi kép: **{Bn:,.2f} triệu đồng**"
-    )
+    st.write(f" Thu nhập chịu thuế: **{thu_nhap:,.2f} triệu đồng**")
+    st.write(f" Thuế TNCN phải nộp: **{thue:,.2f} triệu đồng**")
+    st.write(f" Thu nhập sau thuế: **{thu_nhap_sau_thue:,.2f} triệu đồng**")
